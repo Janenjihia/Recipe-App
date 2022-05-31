08c177f502c144b00b7e51d38e544eb63ca68909
Janenjihia <jane.student@moringaschool.com>
2022-05-31 08:35:52 +0300

    Add Toast and Click listener

--- app/src/main/java/com/moringaschool/recipe/MainActivity.java
+++ app/src/main/java/com/moringaschool/recipe/MainActivity.java
@@ -1,4 +1,32 @@
 package com.moringaschool.recipe;
 
-public class MainActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity  extends AppCompatActivity implements View.OnClickListener{
+    private Button mFindRecipeButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mFindRecipeButton = (Button) findViewById(R.id.findRecipeButton);
+        mFindRecipeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "We ensure you get your craving", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
 }
