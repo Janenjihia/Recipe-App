500ff5c463ae7c19b29e0512c8eb10821245f7e2
Janenjihia <jane.student@moringaschool.com>
2022-05-31 08:54:11 +0300

    Add Adding EditText View

--- app/src/main/java/com/moringaschool/recipe/MainActivity.java
+++ app/src/main/java/com/moringaschool/recipe/MainActivity.java
@@ -2,15 +2,20 @@ package com.moringaschool.recipe;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
-public class MainActivity  extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity  extends AppCompatActivity implements View.OnClickListener {
     private Button mFindRecipeButton;
+    public static final String TAG = MainActivity.class.getSimpleName();
+    private EditText mLocationEditText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,17 +24,23 @@ public class MainActivity  extends AppCompatActivity implements View.OnClickList
 
         mFindRecipeButton = (Button) findViewById(R.id.findRecipeButton);
         mFindRecipeButton.setOnClickListener(new View.OnClickListener() {
+
+
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "We ensure you get your craving", Toast.LENGTH_LONG).show();
                 Intent intent = new Intent(MainActivity.this, RecipeActivity.class);
                 startActivity(intent);
-             }
+            }
         });
     }
 
     @Override
     public void onClick(View v) {
+        String location = mLocationEditText.getText().toString();
+        Log.d(TAG, location);
+        Intent intent = new Intent(MainActivity.this, RecipeActivity.class);
+        startActivity(intent);Toast.makeText(MainActivity.this, location, Toast.LENGTH_LONG).show();
 
     }
 }
