f517f763b07e070b1c009e04abd0c0c1a3df3369
Janenjihia <jane.student@moringaschool.com>
2022-05-31 10:30:15 +0300

    Add Array adapter

--- app/src/main/java/com/moringaschool/recipe/RecipeActivity.java
+++ app/src/main/java/com/moringaschool/recipe/RecipeActivity.java
@@ -2,13 +2,36 @@ package com.moringaschool.recipe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
 
 public class RecipeActivity extends AppCompatActivity {
+    private TextView mLocationTextView;
+    private ListView mListView;
+    private String[] recipes = new String[]{"Chicken Curry", "Pasta", "Fried Beef", "mixed veg Rice",
+            "Chinese rice", "Meat roll", "Pilau", "Tuna", "Mashed potatoes", "Pizza", " Mixed veges", "baked beans",
+            "Beef Tacos", "grilled chicken"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
+
+        mListView = (ListView) findViewById(R.id.listView);
+        mLocationTextView = (TextView) findViewById(R.id.locationTextView);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, recipes);
+        mListView.setAdapter(adapter);
+
+        Intent intent = getIntent();
+        String location = intent.getStringExtra("location");
+        mLocationTextView.setText("Here are all the recipes: " + location);
+
+
     }
 }
