435e7db6d407d9c79502b56da0550620e7a71b6f
Janenjihia <jane.student@moringaschool.com>
2022-05-31 08:39:09 +0300

    Add intent

--- app/src/main/java/com/moringaschool/recipe/MainActivity.java
+++ app/src/main/java/com/moringaschool/recipe/MainActivity.java
@@ -1,5 +1,6 @@
 package com.moringaschool.recipe;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -21,7 +22,9 @@ public class MainActivity  extends AppCompatActivity implements View.OnClickList
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "We ensure you get your craving", Toast.LENGTH_LONG).show();
-            }
+                Intent intent = new Intent(MainActivity.this, RecipeActivity.class);
+                startActivity(intent);
+             }
         });
     }
 
