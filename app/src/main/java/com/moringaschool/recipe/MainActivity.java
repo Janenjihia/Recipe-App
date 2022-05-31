4025060024a739105e016a374cebe8c65bc3b916
Janenjihia <jane.student@moringaschool.com>
2022-05-31 10:45:14 +0300

    implement butterknife

--- app/src/main/java/com/moringaschool/recipe/MainActivity.java
+++ app/src/main/java/com/moringaschool/recipe/MainActivity.java
@@ -6,40 +6,67 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
-public class MainActivity  extends AppCompatActivity implements View.OnClickListener {
-    private Button mFindRecipeButton;
-    public static final String TAG = MainActivity.class.getSimpleName();
-    private EditText mLocationEditText;
 
+public class MainActivity  extends AppCompatActivity {
+//    @BindView(R.id.findRecipeButton) Button mFindRecipeButton;
+//    @BindView(R.id.appNameTextView) TextView mAppNameTextView;
+////    @BindView(R.id.locationEditText) EditText mLocationEditText;
+//    private EditText mLocationEditText;
+//
+//    public static final String TAG = MainActivity.class.getSimpleName();
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        ButterKnife.bind(this);
+//
+//        mFindRecipeButton = (Button) findViewById(R.id.findRecipeButton);
+//        mFindRecipeButton.setOnClickListener(new View.OnClickListener() {
+//
+//
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this, "We ensure you get your craving", Toast.LENGTH_LONG).show();
+//                Intent intent = new Intent(MainActivity.this, RecipeActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//    }
+//
+//    @Override
+//    public void onClick(View v) {
+//        String location = mLocationEditText.getText().toString();
+//        Intent intent = new Intent(MainActivity.this, RecipeActivity.class);
+//        intent.putExtra("location", location);
+//        startActivity(intent);
+//    }
+@BindView(R.id.findRecipeButton) Button mFindRestaurantsButton;
+    @BindView(R.id.cravingEditText) EditText mCravingEditText;
+    @BindView(R.id.appNameTextView) TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        mFindRecipeButton = (Button) findViewById(R.id.findRecipeButton);
-        mFindRecipeButton.setOnClickListener(new View.OnClickListener() {
-
-
+        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "We ensure you get your craving", Toast.LENGTH_LONG).show();
+                String location = mCravingEditText.getText().toString();
                 Intent intent = new Intent(MainActivity.this, RecipeActivity.class);
+                intent.putExtra("location", location);
                 startActivity(intent);
             }
         });
-    }
-
-    @Override
-    public void onClick(View v) {
-        String location = mLocationEditText.getText().toString();
-        Intent intent = new Intent(MainActivity.this, RecipeActivity.class);
-        intent.putExtra("location", location);
-        startActivity(intent);
     }
 }
--- app/src/main/java/com/moringaschool/recipe/RecipeActivity.java
+++ app/src/main/java/com/moringaschool/recipe/RecipeActivity.java
@@ -4,9 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
 
 
 public class RecipeActivity extends AppCompatActivity {
@@ -21,12 +26,21 @@ public class RecipeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
+        ButterKnife.bind(this);
 
-        mListView = (ListView) findViewById(R.id.listView);
+
+        mListView = findViewById(R.id.listView);
         mLocationTextView = (TextView) findViewById(R.id.locationTextView);
 
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, recipes);
         mListView.setAdapter(adapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                String restaurant = ((TextView)view).getText().toString();
+            }
+        });
 
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
